39ae20f27b79ef04f60fbe7abde8326ae09a1f9d
saieshp <saiesh0405@gmail.com>
2023-02-16 23:10:51 +0530

    integrated with weather api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { WeatherService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'weather-test';
+
+  data: any = null;
+  tempInfo: any = null;
+
+  constructor(private weatherService: WeatherService) {
+  }
+
+  ngOnInit(): void {
+    this.getWeather();
+  }
+
+  async getWeather() {
+    const awaitResp = this.weatherService.getWeather();
+    const resp = await lastValueFrom(awaitResp);
+    if (resp) {
+      this.tempInfo = resp;
+    }
+    console.log(resp);
+  }
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+const headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE',
+    'Access-Control-Allow-Origin': '*'
+});
+
+const options = {
+  headers: headers,
+};
+
+@Injectable({ providedIn: "root" })
+export class WeatherService {
+  constructor(private http: HttpClient) {}
+
+  url = "http://localhost:4200/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m";
+
+  getWeather(): Observable<any> {
+    return this.http.get<any>(this.url, options);
+  }
+}
